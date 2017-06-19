41fd5f5cbf1b6d2f30fadd7c14c8166b4c1c1d18
alex <karpovdev@gmail.com>
2017-06-19 21:59:47 +0300

    OAuth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,79 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function loginWithFacebook(Request $request)
+    {
+        // get data from request
+        $code = $request->get('code');
+
+        // get fb service
+        $fb = \OAuth::consumer('Facebook');
+
+        // check if code is valid
+
+        // if code is provided get user data and sign in
+        if ( ! is_null($code))
+        {
+            // This was a callback request from facebook, get the token
+            $token = $fb->requestAccessToken($code);
+
+            // Send a request with it
+            $result = json_decode($fb->request('/me'), true);
+
+            $message = 'Your unique facebook user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
+            echo $message. "<br/>";
+
+            //Var_dump
+            //display whole array.
+            dd($result);
+        }
+        // if not ask for permission first
+        else
+        {
+            // get fb authorization
+            $url = $fb->getAuthorizationUri();
+
+            // return to facebook login url
+            return redirect((string)$url);
+        }
+    }
+
+    public function loginWithGoogle(Request $request)
+    {
+        // get data from request
+        $code = $request->get('code');
+
+        // get google service
+        $googleService = \OAuth::consumer('Google');
+
+        // check if code is valid
+
+        // if code is provided get user data and sign in
+        if ( ! is_null($code))
+        {
+            // This was a callback request from google, get the token
+            $token = $googleService->requestAccessToken($code);
+
+            // Send a request with it
+            $result = json_decode($googleService->request('https://www.googleapis.com/oauth2/v1/userinfo'), true);
+
+            $message = 'Your unique Google user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
+            echo $message. "<br/>";
+
+            //Var_dump
+            //display whole array.
+            dd($result);
+        }
+        // if not ask for permission first
+        else
+        {
+            // get googleService authorization
+            $url = $googleService->getAuthorizationUri();
+
+            // return to google login url
+            return redirect((string)$url);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/loginWithFacebook', 'Auth\LoginController@loginWithFacebook')->name('loginWithFacebook');
+Route::get('/loginWithGoogle', 'Auth\LoginController@loginWithGoogle')->name('loginWithGoogle');
